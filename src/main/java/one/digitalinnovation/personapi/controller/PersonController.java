2fb5a871a5f2bcab62559e5bdaae839a090af3cc
Marcos Freire <marcosruizmrf@outlook.com>
2021-06-19 17:03:42 -0300

    Creating the project and testing the API

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/person")
+public class PersonController {
+
+    @GetMapping
+    public String book() {
+        return "Online";
+    }
+
+
+}
